2a756208e91a2724ea17af169f813f150a5ee4fb
Omar B <bay_omar@yahoo.com>
2023-02-04 20:16:19 -0800

    Second Commit - Adding changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
+import Members from '../views/Members.vue'
+
 
 Vue.use(VueRouter)
 
@@ -13,11 +17,26 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  },
+  {
+    path: '/members',
+    name: 'members',
+    component: Members
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+ 
+ <div>
+  <v-app-bar :elevation="2" rounded class="d-flex justify-space-between">
+         <!-- Using to="/about" on the btn tell Vuetify to link to the -->
+         <!-- routing component -->
+       <v-btn to="/" variant="outlined" class="mr-2">Home</v-btn>
+       <v-btn to="/about" variant="outlined" class="mr-2">About</v-btn>
+       <v-btn to="/contact" variant="outlined" class="mr-2">Contact</v-btn>
+       <v-btn to="/members" variant="outlined" class="mr-2">Members</v-btn>
+     
+     </v-app-bar>
+     <h1>This is an about page</h1>
+   
+ </div>
+ </template>
+ 
